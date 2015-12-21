68d1e442be0bdd4fe8b3bf5a9836086425376aa2
Craig Phares <craigphares@sixoverground.com>
2015-12-21 10:50:14 -0500

    switched to pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151214185614) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notifications", force: :cascade do |t|
     t.text     "certificate"
     t.string   "passphrase"
